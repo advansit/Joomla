c7a19fd882d23394af48bf2d8de424fb03cc0e05
Advans IT Solutions GmbH <no-reply@ona.com>
2026-01-20 17:24:14 +0000

    Add Joomla! AJAX Forms plugin v1.0.0
    AJAX handling for Joomla core forms (password reset, username reminder).
    Returns JSON responses for seamless UX without page reloads.
    
    Features:
    - Password reset via AJAX
    - Username reminder via AJAX
    - J2Commerce compatible error format
    - CSRF protection
    - Email enumeration prevention
    - EN/DE translations
    
    Co-authored-by: Ona <no-reply@ona.com>

---
+++ plg_ajax_joomlaajaxforms/services/provider.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Ajax.JoomlaAjaxForms
+ *
+ * @copyright   Copyright (C) 2025-2026 Advans IT Solutions GmbH. All rights reserved.
+ * @license     Proprietary License - See LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use Advans\Plugin\Ajax\JoomlaAjaxForms\Extension\JoomlaAjaxForms;
+
+return new class () implements ServiceProviderInterface {
+    /**
+     * Registers the service provider with a DI container.
+     *
+     * @param   Container  $container  The DI container.
+     *
+     * @return  void
+     */
+    public function register(Container $container): void
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container) {
+                $plugin = new JoomlaAjaxForms(
+                    $container->get(DispatcherInterface::class),
+                    (array) PluginHelper::getPlugin('ajax', 'joomlaajaxforms')
+                );
+                $plugin->setApplication(Factory::getApplication());
+
+                return $plugin;
+            }
+        );
+    }
+};
